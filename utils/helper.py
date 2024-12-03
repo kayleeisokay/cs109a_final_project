48856b2d382b9c76c31c6d350b495ad409edfb6a
Kaylee Vo <kayleeyvo@gmail.com>
2024-12-02 22:51:23 -0800

    made todo list

--- utils/helper.py
+++ utils/helper.py
@@ -1,6 +1,17 @@
 import numpy as np
+import pandas as pd
 
-def get_variable_category(df):
+def get_variable_category(df: pd.DataFrame) -> tuple:
+    """Helps to organize variables better
+
+    Args:
+        df (pd.DataFrame): main dataframe
+
+    Returns:
+        tuple: columns of the data frame categorized into 
+            discrete, categorical, continuous, binary, 
+            and id variables
+    """
     discrete_vars = []
     categorical_vars = []
     continuous_vars = []
@@ -11,27 +22,35 @@ def get_variable_category(df):
         unique_values = df[col].nunique()
         dtype = df[col].dtype
         
-        # ID variables
+        # IDs
         if unique_values == len(df):
             id_vars.append(col)
         
-        # Binary variables
+        # binary variables
         elif unique_values == 2:
             binary_vars.append(col)
         
-        # Categorical variables
+        # categorical variables
         elif dtype == 'object':
             categorical_vars.append(col)
         
-        # Continuous variables
+        # continuous variables
         elif np.issubdtype(dtype, np.number) and unique_values > 10:
             continuous_vars.append(col)
         
-        # Discrete variables
+        # discrete variables
         elif np.issubdtype(dtype, np.number):
             discrete_vars.append(col)
     
     return discrete_vars, categorical_vars, continuous_vars, binary_vars, id_vars
 
-def split_snake_case(s):
+def split_snake_case(s: str) -> str:
+    """_summary_
+
+    Args:
+        s (str): input string in snake case
+
+    Returns:
+        str: output string in title case
+    """
     return s.replace('_', ' ').title()
